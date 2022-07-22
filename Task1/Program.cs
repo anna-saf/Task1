da424e10da9a616b479b0db68f766138880ecd9a
anna-saf <safonova.irage@yandex.ru>
2022-07-22 23:42:57 +0300

    Final
    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 1;
+            int y = 2;
+            int z = 3;
+            ChangeParams(ref x, ref y);
+            ChangeParams(ref y, ref z);
+
+            Console.WriteLine(String.Format("x={0}, y={1}, z={2}", x, y, z));
+        }
+
+        private static void ChangeParams(ref int param1, ref int param2)
+        {
+            param1 = param1 + param2;
+            param2 = param1 - param2;
+            param1 = param1 - param2;
+        }
+    }
+}
